6ac394657b300121a6fd3d2c574eff2b9a44c866
AlexisDominguez <alexisarieldominguezsanchez@pop-os.localdomain>
2020-02-13 20:33:12 -0700

    Proyecto mudado a Arrow Functions

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
 document.querySelector("#txtBtn").addEventListener("click", cargarTexto);
 document.querySelector("#jsonBtn").addEventListener("click", cargarJSON);
+document.querySelector("#apiBTN").addEventListener("click", cargarREST);
 
 function cargarTexto(){
    fetch("datos.txt")
-      .then(function(response){
-         return response.text();
-      })
-      .then(function (data){
-         document.querySelector("#resultado").innerHTML = data;
-      }).catch(function (error){
-         console.log(error);
-      });
+      .then(response => response.text())
+      .then(data => document.querySelector("#resultado").innerHTML = data)
+      .catch(error => console.log(error));
 }
 
 function cargarJSON(){
    fetch("empleados.json")
-      .then(function(response){
-         return response.json();
-      })
-      .then(function(data){
+      .then(response => response.json())
+      .then(data => {
          const resultado = document.querySelector("#resultado");
          resultado.innerHTML = "";
          for(let i = 0, n = data.length; i<n; i++){
@@ -26,8 +20,30 @@ function cargarJSON(){
             ul.innerHTML = `
                Nombre: ${data[i].nombre}<br>
                Puesto: ${data[i].puesto}<br>
-            `
+            `;
             resultado.appendChild(ul);
          }
       })
+}
+
+// Carga una REST API mediante FETCH
+function cargarREST(){
+   fetch("https://picsum.photos/list")    // REST
+      .then(response => response.json())  // Respuesta del FETCH
+      .then(data => {               // Datos retornados del response
+         const resultado = document.querySelector("#resultado");
+         resultado.innerHTML = "";
+
+         // Imprimero los datos JSON de la API REST en el documento
+         for(let i = 0, n = data.length; i<n; i++){
+            const ul = document.createElement("ul");
+            ul.innerHTML = `
+               Autor: ${data[i].author} <br>
+               Url  : <a href="${data[i].post_url}">${data[i].post_url}</a><br>
+            `;
+
+            resultado.appendChild(ul);
+         }
+      })
+      .catch(error => console.log(error));    // Captura un error en caso de ser necesario
 }
